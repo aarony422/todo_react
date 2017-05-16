66c46800238f330e03b314547ba37f9ced4450c1
Aaron Young <aarony422@gmail.com>
2017-05-15 22:01:03 -0500

    update data on the server with fetch

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import {TodoForm, TodoList, Footer} from './components/todo'
 import {addTodo, generateId, findById, toggleTodo, updateTodo, removeTodo, filterTodos} from './lib/todoHelpers'
 import {pipe, partial} from './lib/utils'
-import {loadTodos, createTodo} from './lib/todoService'
+import {loadTodos, createTodo, saveTodo} from './lib/todoService'
 
 class App extends Component {
   // property intializer syntax in ES6 classes
@@ -33,9 +33,13 @@ class App extends Component {
   // Thus, we don't need a custom constructor
 
   handleToggle = (id) => {
-    const getUpdatedTodos = pipe(findById, toggleTodo, partial(updateTodo, this.state.todos))
-    const updatedTodos = getUpdatedTodos(id, this.state.todos)
+    const getToggledTodo = pipe(findById, toggleTodo)
+    const updated = getToggledTodo(id, this.state.todos)
+    const getUpdatedTodos = partial(updateTodo, this.state.todos)
+    const updatedTodos = getUpdatedTodos(updated)
     this.setState({todos: updatedTodos})
+    saveTodo(updated)
+      .then(() => this.showTempMessage('Todo Updated'))
   }
 
   // optimistic UI update: updating state without waiting for response from server
