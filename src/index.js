fa9a006b909522c67f3d449dca0e4f1f6ab99b28
Aaron Young <aarony422@gmail.com>
2017-05-27 18:53:59 -0500

    using Provider from react-redux

--- src/index.js
+++ src/index.js
@@ -4,26 +4,7 @@ import App from './App';
 import './index.css';
 import { createStore } from 'redux';
 import { todoApp } from './lib/todoStore';
-
-class Provider extends React.Component {
-  // make store available to all children (and grand children) components using
-  // Context feature. The store was passed in via props in our ReactDOM render call
-  getChildContext() {
-    return {
-      store: this.props.store
-    }
-  }
-
-  render() {
-    return this.props.children;
-  }
-}
-
-// Define the context types that children will receive
-// Types must match with those returned in getChildContext
-Provider.childContextTypes = {
-  store: React.PropTypes.object
-}
+import { Provider } from 'react-redux'
 
 ReactDOM.render(
   <Provider store={createStore(todoApp)}>
