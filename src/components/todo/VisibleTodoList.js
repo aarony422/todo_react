a5a42df48a44a6eb799b1c6083eedd0f96877646
Aaron Young <aarony422@gmail.com>
2017-05-27 19:12:23 -0500

    generate containers with connect from react-redux

--- src/components/todo/VisibleTodoList.js
+++ src/components/todo/VisibleTodoList.js
@@ -1,43 +1,34 @@
-import React from 'react'
 import { getVisibleTodos } from '../../lib/todoHelpers'
 import { TodoList } from './TodoList'
+import { connect } from 'react-redux'
 
-export class VisibleTodoList extends React.Component {
-  componentDidMount() {
-    const { store } = this.context;
-    this.unsubscribe = store.subscribe(() =>
-      this.forceUpdate()
-    );
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  render() {
-    const { store } = this.context;
-    const state = store.getState();
-
-    return (
-      <TodoList
-        todos={
-          getVisibleTodos(
-            state.todos,
-            state.visibilityFilter
-          )
-        }
-        onTodoClick={id =>
-          store.dispatch({
-            type: 'TOGGLE_TODO',
-            id
-          })
-        }
-      />
+// take state of the redux store, and returns the props that the presentational
+// components will use. These props will be updated anytime the state changes
+const mapStateToProps = (state) => {
+  return {
+    todos: getVisibleTodos(
+      state.todos,
+      state.visibilityFilter
     )
   }
 }
 
-// Define context types that will be received from parent
-VisibleTodoList.contextTypes = {
-  store: React.PropTypes.object
+// takes stores dispatch function, and returns the props that will use the dispatch
+// method to dispatch actions.
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onTodoClick: (id) => {
+      dispatch({
+        type: 'TOGGLE_TODO',
+        id
+      })
+    }
+  }
 }
+
+// use connect to create a container component
+export const VisibleTodoList = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList); // curried function, to pass in the presentational component that
+// will connect to the redux store
