314d46191a7a50276d2693bb8195439ecd840eaa
Aaron Young <aarony422@gmail.com>
2017-05-29 14:51:06 -0700

    refactored FilterLink to use connect

--- src/components/router/FilterLink.js
+++ src/components/router/FilterLink.js
@@ -1,46 +1,26 @@
 import React from 'react'
 import {Link} from './Link'
+import { connect } from 'react-redux'
 
-// Container Component that provides the data and behavior for Link, the presentational
-// component. When FilterLink mounts, it subscribes to the store, and re-renders
-// when the store changes. This is because this component actually uses the store's state
-// when rendering. We then delegate the rendering to Link.
-export class FilterLink extends React.Component {
-  componentDidMount() {
-    const { store } = this.context;
-    this.unsubscribe = store.subscribe(() =>
-      this.forceUpdate()
-    );
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  render() {
-    const props = this.props;
-    const { store } = this.context;
-    const state = store.getState();
-
-    return (
-      <Link
-        active={
-          props.filter === state.visibilityFilter
-        }
-        onClick={() => {
-          store.dispatch({
-            type: 'SET_VISIBILITY_FILTER',
-            filter: props.filter
-          })
-        }}
-      >
-      {props.children}
-      </Link>
-    );
+// props of the child prop is passed in as the second parameter
+const mapStateToProps = (state, ownProps) => {
+  return {
+    active: ownProps.filter === state.visibilityFilter
   }
 }
 
-// Define context types that will be received from parent
-FilterLink.contextTypes = {
-  store: React.PropTypes.object
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    onClick: () => {
+      dispatch({
+        type: 'SET_VISIBILITY_FILTER',
+        filter: ownProps.filter
+      })
+    }
+  }
 }
+
+export const FilterLink = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Link);
